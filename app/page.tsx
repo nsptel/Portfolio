85ac4bbb79eee6c7a1407a215f0d6d4fd381c39c
Nilkumar Patel <nilkumarpatel48@gmail.com>
2023-01-26 19:00:23 -0500

    Update greeting to make it bolder, better

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 function Home(): React.ReactElement {
   return (
     <>
-      <div className="bg-white">
-        <h1 className="text-black">Portfolio - nsptel</h1>
+      <div className="bg-black h-screen w-screen flex justify-evenly flex-row items-center">
+        <h1 className="text-white text-6xl font-extrabold text-center">
+          Hi, this is Neel.
+        </h1>
       </div>
     </>
   );
