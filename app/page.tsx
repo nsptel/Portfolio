9d702fd934fdfb1751eeab88f04fc459e690c2fe
Nilkumar Patel <nilkumarpatel48@gmail.com>
2023-02-26 13:10:49 -0500

    Format files, change main text color

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ function Home(): React.ReactElement {
   return (
     <>
       <div className="bg-black h-screen w-screen flex justify-evenly flex-row items-center">
-        <h1 className="text-white text-6xl font-extrabold text-center">
+        <h1 className="text-6xl text-white font-extrabold">
           Hi, this is Neel.
         </h1>
       </div>
