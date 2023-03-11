e1a27c8f9bfc5e7258197ab8299bac85e67b901b
Nilkumar Patel <nilkumarpatel48@gmail.com>
2023-03-10 22:17:31 -0500

    Update text color to light gray

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ function Home(): React.ReactElement {
   return (
     <>
       <div className="bg-black h-screen w-screen flex justify-evenly flex-row items-center">
-        <h1 className="text-6xl text-white font-extrabold">
+        <h1 className="text-6xl text-gray-300 font-extrabold">
           Hi, this is Neel 👋
         </h1>
       </div>
